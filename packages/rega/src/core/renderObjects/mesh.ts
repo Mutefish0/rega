0a0256b32cc72d540d91792fbea71bac3d5d0a31
mutefish0 <648262030@qq.com>
2024-09-26 18:36:40 +0800

    add types

---
+++ packages/rega/src/core/renderObjects/mesh.ts
@@ -0,0 +1,12 @@
+// mesh  ----- matrix
+// mesh ----- order
+// mesh ----- geometry
+// mesh ----- material
+
+// geometry
+// geometry --- atributtes
+
+// material
+// material -- uniforms
+// material -- vertexShader
+// material -- fragmentShader
